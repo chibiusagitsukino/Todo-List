980ee5daa3d4d5c8107b7d00953a76b8f799ae63
Adriana Origlio <adrianaoriglio@gmail.com>
2023-04-27 13:05:35 +0200

    fixed style

--- src/mock/todolist.js
+++ src/mock/todolist.js
@@ -1,32 +1,32 @@
 const todoList = [
   {
     id: 1,
-    text: 'Ordinare la spesa',
+    text: 'Ordering the groceries',
     completed: false,
   },
   {
     id: 2,
-    text: 'Stendere il bucato',
+    text: 'Hanging out the laundry.',
     completed: false,
   },
   {
     id: 3,
-    text: 'Fare la pulizia della casa',
+    text: 'Cleaning the house',
     completed: false,
   },
   {
     id: 4,
-    text: 'Preparare la cena',
+    text: 'Cooking the dinner',
     completed: false,
   },
   {
     id: 5,
-    text: 'Studiare React',
+    text: 'Learning React.js',
     completed: false,
   },
   {
     id: 6,
-    text: 'Iniziare un nuovo romanzo',
+    text: 'Starting a new book',
     completed: false,
   },
 ]
